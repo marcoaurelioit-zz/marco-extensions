5a5633f35532c052fe626997c35fe856d842c430
Marco Aurélio <maureliobrasil@gmail.com>
2018-10-08 13:49:19 -0300

    Update MarcoExtensionsConfiguration.cs

--- src/Microsoft.Extensions.Configuration/MarcoExtensionsConfiguration.cs
+++ src/Microsoft.Extensions.Configuration/MarcoExtensionsConfiguration.cs
@@ -17,14 +17,14 @@ namespace Microsoft.Extensions.Configuration
             T model = configuration.Get<T>();
 
             if (model == null)
-                throw new InvalidOperationException($"Item de configuração não encontrado para o tipo {typeof(T).FullName}.");
+                throw new InvalidOperationException($"Configuration item not found for type {typeof(T).FullName}.");
 
             var validationResults = new List<ValidationResult>();
             var validationContext = new ValidationContext(model, serviceProvider: null, items: null);
 
             if (!Validator.TryValidateObject(model, validationContext, validationResults, true))
             {
-                string message = $"A validação de configuração para o tipo '{model.GetType().FullName}' falhou: " +
+                string message = $"Configuration validation for type '{model.GetType().FullName}' failed: " +
                 string.Join(", ", validationResults.Select(r => $"\"{r.ErrorMessage}\""));
 
                 throw new InvalidOperationException(message);
